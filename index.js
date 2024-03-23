a9ccdf51444e6121d23c156ce755317aa6b17e1e
M Ifraheem <mifrahim017@gmail.com>
2024-03-23 16:32:38 +0500

    static files config

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const io = new Server(server);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("./public"));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("chat.ejs");
 });
